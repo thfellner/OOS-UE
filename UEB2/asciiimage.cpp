bb8d6a7a4df14f8880fbd95bff517cdebf718455
Thomas Fellner <fellnertho@gmail.com>
2019-03-20 10:41:21 +0100

    add asciiimage basic structure

--- UEB2/asciiimage.cpp
+++ UEB2/asciiimage.cpp
@@ -1,4 +1,20 @@
 //
 // Created by tfellner on 20.03.19.
 //
+#include "asciiimage.h"
 
+myAscii::myAscii(int width, int height) {
+
+}
+
+myAscii::~myAscii() {
+    
+}
+
+void myAscii::setPix(int x, int y, int color) {
+
+}
+
+void myAscii::show() {
+
+}
--- UEB2/main.cpp
+++ UEB2/main.cpp
@@ -1,4 +1,15 @@
 //
 // Created by tfellner on 20.03.19.
 //
+#include <string>
+#include <iostream>
 
+
+int main() {
+    std::string characters = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^'. ";
+
+    std::string altChracters = " ░▕▏▕▎▍▁▔▂▖▘▝▗▃▌▄▐▋▅▆▇▊▀▒▓▞▙▚▛▜▟█▉";
+    std::string alt2Characters = "▉█▟▜▛▚▙▞▓▒▀▊▇▆▅▋▐▄▌▃▗▝▘▖▂▔▁▍▎▕▏▕░ ";
+
+
+}
